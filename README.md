42c696f327ba940802b7c9a7c7a8f9c0ab13711e
Filipe Rey <f.reysantos7@gmail.com>
2023-05-19 02:11:30 +0100

    Update on Hardhat scripts / tests /contracts

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
-We can say that “safe and “secure” are the words that come to mind when speaking about web3 wallets. 
-Concerns regarding security are a primary thought when it comes to web3 systems, and that's why smart contracts play a significan role in every Dapp. These allow to mitigate the risks brought upon by bad actors and assure the safety of your funds.
+This paper introduces **Spades**: an open source multisig wallet contract.
 
-There are several projects in the ecosystem that already implement great Multi-sig wallets, that can be super reliable when talking about others stealing your funds.
+ **Spades** is a smart contract, that uses multisignature as its core feature. Users that deploy this contract, can choose how many owners/signatures will be required.
 
-The objective with this project, is to try to build a Multi-sig wallet with my knowledge, and experience new features, that other wallets on Ethereum may not have.
 
---
+++ contracts/Spades.sol
@@ -0,0 +1,106 @@
+// SPDX-License-Identifier: MIT
+pragma solidity: "0.8.18";
+
+import "hardhat/console.sol";
+
+contract MultiSigWallet {
+    
+
+          // Emits a Deposit //
+
+    event Deposit (address sender, uint value);
+
+        // Transaction is submited //
+
+    event Submit (address _to, uint _value, bool executed);
+
+         // Tracks a transaction //
+
+    struct Transaction {
+        address to;
+        uint value;
+        bool executed;
+    }
+     
+         // Stores owners addresses //
+
+    address[] public owners;
+    mapping(address => bool) public OwnersCheck;
+
+         // Stores the required Signatures //
+
+    uint public requiredSignatures;
+
+    Transaction[] public transactions;
+    maaping(uint => mapping(address => bool)) public approved;
+
+
+        // CHecks if msg.sender is owner //
+
+    modifier ownerOnly() {
+        address owners[owner] = msg.sender, "Not owner";
+        _;
+    }true
+
+        // Checks if transaction was executed //
+
+    modifier txExists(_txIndex) {
+        require(_txIndex < transactions.length), "Transaction doesn't exist";
+        _;
+    }
+
+    modifier notSigned(_txIndex) {
+        require(!txSigned)[_txIndex][msg.sender];
+        _;
+    }
+
+    modifier notExecuted() {
+        require(bool executed == false);
+    }
+
+
+          // Sets the number of owners and signatures needed //
+    constructor(address[] memory _owners, uint _signaturesRequired) {
+        require(_owners.length > 0, "Not enough owners");
+        require(_signaturesRequired > 0 && _signaturesRequired <= _owners.length, "Invalid, due to number of owners");
+
+       for (uint i; i < _owners.length; i ++) {
+        address owner = _owners[i];
+        require(owner != address(0), "Invalid owner");
+        require(! OwnersCheck[owner], "Owner not unique");
+        OwnersCheck[owner] = true;
+        owners.push(owner);
+       }
+       
+       requiredSignatures = _signaturesRequired;
+    }
+    
+        // Receive Ether //
+    receive() external payable {
+        emit Deposit (msg.sender, msg.value);
+    }
+
+        // Submits a transaction //
+    function submit(address _to, uint value, bytes calldata _data) external ownerOnly {
+        uint txIndex = transactions.length;
+
+        transactions.push(
+            Transaction({
+                to: _to;
+                value: _value;
+                data: _data;
+                executed: false,
+                numConfirmations: 0
+            })
+        );
+
+    function signTransaction(uint _txIndex) public ownerOnly txExists(_txIndex) notSigned(txIndex){
+        Transaction storage transaction = transactions[_txIndex];
+        transaction.numConfirmations += 1;
+        txSigned[_txIndex][msg.sender] = true;
+
+        
+    }
+    
+
+}
