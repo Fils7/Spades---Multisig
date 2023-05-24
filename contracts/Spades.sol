c98aaa6106ba22e892dfbc7214793b59051b79f9
Filipe Rey <f.reysantos7@gmail.com>
2023-05-24 11:42:06 +0100

    Simplify contract base and TODO's list;

--- contracts/Spades.sol
+++ contracts/Spades.sol
@@ -5,88 +5,92 @@ import "hardhat/console.sol";
 
 contract MultiSigWallet {
     
-          // Emits a Deposit //
-    event Deposit (address sender, uint value);
+// Emits a Deposit 
+    event Deposit (address sender, uint _value);
 
-        // Transaction is submited //
-    event Submit (address _to, uint _value, bool executed);
+// Transaction is submited 
+    event Submit (address _to, uint _value);
 
-        // Transaction Executed //
+    // TODO: Sign event.
+ // Signs a Transaction
+    event Sign (address indexed owner);
 
+// Transaction Executed 
     event transactionExecuted (address indexed owner, uint indexed tx_Index);
 
-         // Tracks a transaction //
+// Tracks a transaction 
     struct Transaction {
         address to;
         uint value;
         uint confirmations;
-        bool executed;
+        // TODO: Map of who signed!!!
+        address signature;
+
     }
     
-         // Stores owners addresses //
+// Stores owners addresses 
     address[] public owners;
     mapping(address => bool) public OwnersCheck;
 
-         // Stores the required Signatures //
+// Stores the required Signatures
     uint public requiredSignatures;
 
-        // Stores the time frame after confirmation //
-    uint public timeFrame;
+    // TODO: Change to mapping and create a global nonce to store the next tx index.
+    mapping(uint => Transaction) public nonce;
+    uint nonceNumber;
 
-    Transaction[] public transactions;
-
-
-        // CHecks if msg.sender is owner //
+// Checks if msg.sender is owner 
     modifier ownerOnly() {
         require(OwnersCheck[msg.sender], "Not owner");
         _;
     }
 
-        // Checks if transaction exists //
+// Checks if transaction exists 
     modifier txExists(uint _txIndex) {
-        require(_txIndex < transactions.length, "Tx doesn't exist");
+        require(_txIndex < nonce.length, "Tx doesn't exist");
         _;
     }
 
-          // Sets the number of owners and signatures needed //
+// Sets the number of owners and signatures needed 
     constructor(address[] memory _owners, uint _signaturesRequired) {
         require(_owners.length > 0, "Not enough owners");
         require(_signaturesRequired > 0 && _signaturesRequired <= _owners.length, "Invalid, due to number of owners");
 
        for (uint i; i < _owners.length; i ++) {
         address owner = _owners[i];
-        require(owner != address(0), "Invalid owner");
         require(! OwnersCheck[owner], "Owner not unique");
         OwnersCheck[owner] = true;
         owners.push(owner);
        }
-       
+
        requiredSignatures = _signaturesRequired;
     }
     
-        // Receive Ether //
+// Receive Ether 
     receive() external payable {
         emit Deposit (msg.sender, msg.value);
     }
 
-        // Submits a transaction //
+// Submits a transaction 
     function submit(address _to, uint _value) external ownerOnly {
-        uint txIndex = transactions.length;
+        nonceNumber += 1;
+        Transaction({
+            to: _to,
+            value: _value,
+                // TODO: Should already have one signature.
+            confirmations: 1,
+            signature: msg.sender
+        })
 
-        transactions.push(
-            Transaction({
-                to: _to,
-                value: _value,
-                confirmations: 0,
-                executed: false
-            })
-        );
-    }
-
+    ;}
+    
     function signTransaction(uint _txIndex) public ownerOnly txExists(_txIndex){
-        Transaction storage transaction = transactions[_txIndex];
+        // TODO: Check if the same owner already signed!!!
+        require(! msg.sender.Submit);
+        Transaction storage transaction = Transaction[_txIndex];
         transaction.confirmations += 1;
     }
     
+        // TODO: Execute function
 
 }
